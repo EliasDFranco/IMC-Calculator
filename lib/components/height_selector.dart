9e90df07e658ab104d149235276d08d13448ab46
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-06-01 13:58:40 -0700

    Modificando algunas partes del heightSelector

--- lib/components/height_selector.dart
+++ lib/components/height_selector.dart
@@ -22,7 +22,10 @@ class _HeightSelectorState extends State<HeightSelector> {
         ),
         child: Column(
           children: [
-            Text("ALTURA", style: TextStyles.bodytext),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text("ALTURA", style: TextStyles.bodytext),
+            ),
             Text("${height.toStringAsFixed(0)} cm ",
                 style: TextStyle(
                     color: Colors.white,

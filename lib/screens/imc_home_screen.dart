b109871b442fd76cd9d0c6ebd40441bbed469295
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-06-05 13:47:50 -0700

    Devolviendo parámetros | personalizando botones

--- lib/screens/imc_home_screen.dart
+++ lib/screens/imc_home_screen.dart
@@ -1,16 +1,20 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:imc_calculator/components/gender_selector.dart';
 import 'package:imc_calculator/components/height_selector.dart';
 import 'package:imc_calculator/components/number_selector.dart';
+import 'package:imc_calculator/core/apps_colors.dart';
+import 'package:imc_calculator/core/apps_textStyles.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
 
   @override
-  State<ImcHomeScreen> createState() => _ImcHomeScreentState();
+  State<ImcHomeScreen> createState() => _ImcHomeScreenState();
 }
 
-class _ImcHomeScreentState extends State<ImcHomeScreen> {
+class _ImcHomeScreenState extends State<ImcHomeScreen> {
+  int selectorAge = 6;
+  int selectorWeight = 50;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -20,10 +24,52 @@ class _ImcHomeScreentState extends State<ImcHomeScreen> {
         Padding(
           padding: const EdgeInsets.all(12.0),
           child: Row(children: [
-            Expanded(child: NumberSelector(titulo: "PESO")),
+            Expanded(
+                child: NumberSelector(
+                    titulo: "PESO",
+                    value: selectorWeight,
+                    onIncrement: () {
+                      setState(() {
+                        selectorWeight++;
+                      });
+                    },
+                    onDecrement: () {
+                      setState(() {
+                        selectorWeight--;
+                      });
+                    })),
             SizedBox(width: 16),
-            Expanded(child: NumberSelector(titulo: "EDAD"))
+            Expanded(
+                child: NumberSelector(
+                    titulo: "EDAD",
+                    value: selectorAge,
+                    onIncrement: () {
+                      setState(() {
+                        selectorAge++;
+                      });
+                    },
+                    onDecrement: () {
+                      setState(() {
+                        selectorAge--;
+                      });
+                    }))
           ]),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+              height: 55,
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                      backgroundColor:
+                          MaterialStateProperty.all(AppColors.primary)),
+                  child: const Text("Calcular tu masa corporal ideal",
+                      style: TextStyles.bodytext))),
         ),
       ],
     );
